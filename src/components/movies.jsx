9f5fc2feb49e05405ad9c629953a6c280156c2ab
PetePhx <pete0phx@yandex.com>
2018-11-24 16:04:15 -0700

    movies table

---
+++ src/components/movies.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+
+class Movies extends Component {
+  state = {
+    movies: getMovies()
+  };
+
+  handleDelete = movieIdx => {
+    this.state.movies.splice(movieIdx, 1);
+    this.setState({ movies: this.state.movies });
+  };
+
+  render() {
+    const len = this.state.movies.length;
+    if (len === 0) return <p>There are no movies in the database!</p>;
+    return (
+      <React.Fragment>
+        <p>Showing {len} movies:</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map((movie, idx) => (
+              <tr key={idx}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    className="btn btn-danger btn-sm"
+                    onClick={() => this.handleDelete(idx)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Movies;
