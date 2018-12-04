160bbed897b88d60336ed3127d2012560bc599af
PetePhx <pete0phx@yandex.com>
2018-12-04 13:48:44 -0700

    Extract LoginForm re-usable parts to a common Form class

---
+++ src/components/common/form.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import Joi from "joi-browser";
+
+class Form extends Component {
+  state = {
+    data: {},
+    errors: {}
+  };
+
+  validate = () => {
+    const options = { abortEarly: false };
+    const { error: err } = Joi.validate(this.state.data, this.schema, options);
+    if (!err) return null;
+
+    const errors = {};
+    err.details.forEach(item => (errors[item.path] = item.message));
+    return errors;
+  };
+
+  validateProperty = ({ name, value }) => {
+    const obj = { [name]: value };
+    const schema = { [name]: this.schema[name] };
+    const { error } = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const errors = this.validate();
+    this.setState({ errors: errors || {} });
+    if (errors) return;
+
+    this.doSubmit();
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
+    const data = { ...this.state.data };
+    data[input.name] = input.value;
+
+    this.setState({ data, errors });
+  };
+}
+
+export default Form;
