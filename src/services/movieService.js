afb16c7a5b4b34501236152c50677f6db7566baf
PetePhx <pete0phx@yandex.com>
2018-12-11 09:33:35 -0700

    refactor movie service

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -3,25 +3,26 @@ import { apiUrl } from "../config.json";
 
 const apiEndpoint = apiUrl + "/movies";
 
+function movieUrl(id) {
+  return `${apiEndpoint}/${id}`;
+}
+
 export function getMovies() {
   return http.get(apiEndpoint);
 }
 
 export function deleteMovie(movieId) {
-  return http.delete(apiEndpoint + "/" + movieId);
+  return http.delete(movieUrl(movieId));
 }
 
 export function getMovie(movieId) {
-  return http.get(apiEndpoint + "/" + movieId);
+  return http.get(movieUrl(movieId));
 }
 
 export function saveMovie(movie) {
-  if (movie._id) {
-    const url = apiEndpoint + "/" + movie._id;
-    delete movie._id;
-    return http.put(url, movie);
-  } else {
-    const url = apiEndpoint;
-    return http.post(url, movie);
-  }
+  if (!movie._id) return http.post(apiEndpoint, movie);
+
+  const body = { ...movie };
+  delete body._id;
+  return http.put(movieUrl(movie._id), body);
 }
